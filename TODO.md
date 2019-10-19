aa425b8f4b078c7cb7d408e5800c7ab3d104ca33
Audrow Nash <audrowna@usc.edu>
2019-10-19 15:41:16 -0700

    Create and test functions for calculating daily and weekly steps goals

---
+++ TODO.md
@@ -0,0 +1,5 @@
+TODO
+====
+
+* Make thread safe, use `filelock`
+
--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
   install_requires=[
     'dill',
     'robotpt_common_utils',
+    'filelock',
   ],
   classifiers=[
     'Development Status :: 3 - Alpha',
