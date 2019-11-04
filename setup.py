30c5e6f625fb6c31ef5503b3de1eabe5a60a6dda
Audrow Nash <audrowna@usc.edu>
2019-11-03 17:22:30 -0800

    Update version in setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/robotpt/pickle-database'
 EMAIL = 'audrowna@usc.edu'
 AUTHOR = 'Audrow Nash'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
